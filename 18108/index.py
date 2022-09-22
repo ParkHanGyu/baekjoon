40752140446981d7984c3e229134009a29088284
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2022-09-22 23:32:53 +0900

    폴더 이동

--- 18108/index.py
+++
@@ -1,3 +0,0 @@
-a = int(input())
-b = int("543")
-print(a - b)
--- 2588/index.py
+++
@@ -1,5 +0,0 @@
-a, b = map(int, input().split())
-print(a * (b % 10))
-print(a * (b % 100 // 10))
-print(a * (b % 1000 // 100))
-print(a * b)
