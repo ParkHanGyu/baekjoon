f94255a5904173aa1d5aea16f0b3d80367c8100f
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2022-10-05 22:25:26 +0900

    수정 - 불필요한 요소 삭제

--- 1_array/3052/index.py
+++ 1_array/3052/index.py
@@ -1,8 +1,15 @@
 lst = []
-p = []
-cnt = 0
+
 for i in range(10) :
-    lst.append(int(input()))
-    p.append(int(lst[i] % 42))
-p = set(p)
-print(len(p))
+    n = int(input())
+    lst.append(n % 42)
+lst = set(lst)
+print(len(lst))
+
+# lst = []
+# p = []
+# for i in range(10) :
+#     lst.append(int(input()))
+#     p.append(int(lst[i] % 42))
+# p = set(p)
+# print(len(p))
