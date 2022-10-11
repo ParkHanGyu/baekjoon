46cdc1077fb76eed628a5cf85b70966d2eb9beaa
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2022-10-11 22:30:25 +0900

    평균은 넘겠지 - 반 별로 평균을 구해서 평균을 넘는 인원이 몇퍼센트인지 구하는 문제

--- 1_array/4344/index.py
+++ 1_array/4344/index.py
@@ -1,19 +1,14 @@
-from tkinter import scrolledtext
-
-
 n = int(input())
 
 for i in range(n):
     p_list = list(map(int, input().split()))
     p_list = p_list[:p_list[0] + 1]
     del p_list[0]
-    listSum = sum(p_list) # 점수 합
-    listMean = listSum / len(p_list) # 점수 평균
-    u = listMean / 100.0 * 100.0 # 평균 점수 백분율
-    z = [] # 각 점수별 백분율
+    listMean = sum(p_list) / len(p_list) # 점수 평균
+    mean_over = [] # 평균을 넘는 값
 
     for a in range(len(p_list)):
-        z.append(p_list[a] / 100.0 * 100.0)
-    else:
-        break
-# print(u)
+        if p_list[a] > listMean:
+            mean_over.append(p_list[a] / 100.0 * 100.0)
+
+    print("%.3f%%" % (len(mean_over) / len(p_list) * 100.0))
